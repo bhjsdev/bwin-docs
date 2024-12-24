61b0ab70808c76b56309f6319c7a2571a7a73705
Bhjs Dev <bhjsdev@gmail.com>
2024-12-25 07:45:46 +1100

    fix: build date locale string

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import './footer.css';
 
 export default function Footer() {
   const buildDate = process.env.BUILD_DATE;
+  const localDate = new Date(buildDate).toLocaleString('en-GB', { timeZoneName: 'short' });
 
-  return <footer className="layout__footer">Build date: {buildDate}</footer>;
+  return <footer className="layout__footer">Build date: {localDate}</footer>;
 }
