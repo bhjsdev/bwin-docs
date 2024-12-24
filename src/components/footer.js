fa2c6b14ad2474a65a07d04bae9baff8c88ac1b4
Bhjs Dev <bhjsdev@gmail.com>
2024-12-24 20:13:38 +1100

    feat: update favicon and minor content update

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,5 +3,5 @@ import './footer.css';
 export default function Footer() {
   const buildDate = process.env.BUILD_DATE;
 
-  return <footer className="layout__footer">Updated at: {buildDate}</footer>;
+  return <footer className="layout__footer">Build date: {buildDate}</footer>;
 }
