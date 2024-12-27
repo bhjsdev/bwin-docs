c9a9a5f728a0c2848de961b39afd7139008db37d
Bhjs Dev <bhjsdev@gmail.com>
2024-12-27 16:21:16 +1100

    feat: active side nav link style

--- src/components/sidenav-list.js
+++ src/components/sidenav-list.js
@@ -1,19 +1,33 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function SidenavList({ heading, links, iconClassName }) {
+  const pathname = usePathname();
+
   return (
-    <>
+    <div className="sidenav-list">
       <h2>
         <i className={iconClassName}></i>
         {heading}
       </h2>
       <ul>
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link href={link.href}>{link.title}</Link>
-          </li>
-        ))}
+        {links.map((link) => {
+          const linkClassName =
+            pathname === link.href
+              ? 'sidenav-list__link sidenav-list__link--active'
+              : 'sidenav-list__link';
+
+          return (
+            <li key={link.href}>
+              <Link href={link.href} className={linkClassName}>
+                {link.title}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
-    </>
+    </div>
   );
 }
