f180e3d5f587d810deb53630be3ec1b35bda0b09
Bhjs Dev <bhjsdev@gmail.com>
2025-01-04 11:27:35 +1100

    fix: ssr mismatch warning

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,15 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import './footer.css';
 
 export default function Footer() {
-  const buildDate = process.env.BUILD_DATE;
-  const localDate = new Date(buildDate).toLocaleString('en-GB', { timeZoneName: 'short' });
+  const [buildDate, setBuildDate] = useState(process.env.BUILD_DATE);
 
-  return (
-    <footer className="layout__footer" suppressHydrationWarning={true}>
-      Build date: {localDate}
-    </footer>
-  );
+  // Call useEffect to avoid SSR mismatch
+  useEffect(() => {
+    setBuildDate(new Date(buildDate).toLocaleString('en-GB', { timeZoneName: 'short' }));
+  }, []);
+
+  return <footer className="layout__footer">Build date: {buildDate}</footer>;
 }
