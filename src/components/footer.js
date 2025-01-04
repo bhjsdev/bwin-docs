0f8f67b7f696b239bc7b2b6305949792a3dc600b
Bhjs Dev <bhjsdev@gmail.com>
2025-01-04 11:20:01 +1100

    fix: suppress hydration warning

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,5 +6,9 @@ export default function Footer() {
   const buildDate = process.env.BUILD_DATE;
   const localDate = new Date(buildDate).toLocaleString('en-GB', { timeZoneName: 'short' });
 
-  return <footer className="layout__footer">Build date: {localDate}</footer>;
+  return (
+    <footer className="layout__footer" suppressHydrationWarning={true}>
+      Build date: {localDate}
+    </footer>
+  );
 }
